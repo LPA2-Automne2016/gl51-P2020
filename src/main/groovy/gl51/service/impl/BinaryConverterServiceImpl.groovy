070d3f53da52712645b9caa0fe71014b3a7b948e
sgadot <sebastien.gadot@lisi-automotive.com>
2020-03-13 17:16:43 +0100

    creation

---
+++ src/main/groovy/gl51/service/impl/BinaryConverterServiceImpl.groovy
@@ -0,0 +1,15 @@
+package gl51.service.impl
+
+import gl51.data.Ip
+import gl51.service.IPService
+import gl51.service.IpConverterService
+
+class BinaryConverterServiceImpl implements IpConverterService {
+
+    IPService service = new IPService()
+
+    @Override
+    String getAndConvertIp() {
+        return null
+    }
+}
