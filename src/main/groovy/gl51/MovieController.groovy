31017560482fb2e8323c7e7df340a38605834c99
sgadot <sebastien.gadot@lisi-automotive.com>
2020-04-10 16:11:25 +0200

    nettoyage des imports

--- src/main/groovy/gl51/MovieController.groovy
+++ src/main/groovy/gl51/MovieController.groovy
@@ -3,14 +3,12 @@ package gl51
 import gl51.data.MovieRequest
 import gl51.movie.data.Movie
 import gl51.movie.service.MovieRegistry
-import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Post
 
 import javax.inject.Inject
-
 
 @Controller("/movie")
 class MovieController {
