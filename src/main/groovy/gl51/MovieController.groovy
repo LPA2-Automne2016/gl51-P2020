c66620c289a8255ec2a3f1e76b2bcd88759baa87
sgadot <sebastien.gadot@lisi-automotive.com>
2020-04-10 15:52:30 +0200

    creation du controlleur

--- src/main/groovy/gl51/MovieController.groovy
+++ src/main/groovy/gl51/MovieController.groovy
@@ -2,23 +2,30 @@ package gl51
 
 import gl51.data.MovieRequest
 import gl51.movie.data.Movie
+import gl51.movie.service.MovieRegistry
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Post
 
+import javax.inject.Inject
+
 
 @Controller("/movie")
 class MovieController {
 
+    @Inject
+    MovieRegistry registry
+
     @Get("/")
     List<Movie> index() {
-        []
+        registry.listFavorites()
     }
 
     @Post('/')
     HttpStatus addMovie(MovieRequest movieRequest) {
+        registry.addMovieToFavorites(movieRequest.imdbId)
         HttpStatus.CREATED
     }
 }
